5e38267548c542baef909edce59d834e6f3dca92
jeff <goffredo.gonnella@gmail.com>
2019-06-18 11:03:27 +0100

    worked on items bakery keyboard

--- src/custom_hooks.js/modal_hook.js
+++ src/custom_hooks.js/modal_hook.js
@@ -52,9 +52,6 @@ const useModal = () =>{
                         }else{
                             setproductDetails([...productDetails, ...response.data]);
                             setModal(!modal);
-                        }
-                        
-                    
     
                 });
             });
