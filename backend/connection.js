d71545589449652fbecd0e3690c92329032469a8
jeff <goffredo.gonnella@gmail.com>
2019-08-16 17:35:59 +0100

    test 3

--- backend/connection.js
+++ backend/connection.js
@@ -72,10 +72,9 @@ app.use('/login', loginRoute);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('build'));
 
-    app.get('*', (req, res) =>{
+    app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-}
+      });
 
 app.listen(PORT, () =>{
     console.log('server running on port ' + PORT);
