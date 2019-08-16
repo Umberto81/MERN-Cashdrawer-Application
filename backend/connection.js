010ca4af6108a2eb7f8791a7f296300492233ec0
jeff <goffredo.gonnella@gmail.com>
2019-08-16 22:14:26 +0100

    kjkkj

--- backend/connection.js
+++ backend/connection.js
@@ -71,7 +71,7 @@ app.use('/login', loginRoute);
 
 if(process.env.NODE_ENV === 'production'){
 app.use(express.static(path.join(__dirname, 'build')));
-    app.get('*', function (req, res) {
+    app.get('/*', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
       });
     }
