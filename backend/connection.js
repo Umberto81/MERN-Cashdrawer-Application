fd7fec0c07e291b48490fb00cd491874d173a56b
jeff <goffredo.gonnella@gmail.com>
2019-08-17 12:59:37 +0100

    ,

--- backend/connection.js
+++ backend/connection.js
@@ -73,7 +73,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '../build/index.html'));
+        res.sendFile(path.join(__dirname + 'build/index.html'));
       });
 }
 
