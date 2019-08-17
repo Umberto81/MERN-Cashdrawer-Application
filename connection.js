43a422636eac209cf4e4f7b6df54071e2627029d
jeff <goffredo.gonnella@gmail.com>
2019-08-17 14:23:03 +0100

    k

--- connection.js
+++ connection.js
@@ -72,7 +72,7 @@ app.use('/login', loginRoute);
 
     app.use(express.static(path.join(__dirname, 'build')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 
